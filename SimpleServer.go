af0b1c95db7592ea0880ea8ab1621f3cd2ad99dc
Jaideep Kekre <jaideepkekre@gmail.com>
2019-10-16 11:46:36 +0530

    added endpoint struct, changed to a struct based approach

--- SimpleServer.go
+++ SimpleServer.go
@@ -1,4 +1,5 @@
 // package main
+
 package SimpleServer
 
 import (
@@ -6,9 +7,18 @@ import (
 	"net/http"
 )
 
+type EndPointMux struct {
+	URI    string
+	GET    func(w http.ResponseWriter, r *http.Request)
+	PUT    func(w http.ResponseWriter, r *http.Request)
+	POST   func(w http.ResponseWriter, r *http.Request)
+	DELETE func(w http.ResponseWriter, r *http.Request)
+	PATCH  func(w http.ResponseWriter, r *http.Request)
+}
+
 // Plugs in Auth, supports JWT
-func AuthMiddleware(h http.Handler, comment string) http.Handler {
-	log.Println("Inflating AuthMiddleWare  for " + comment)
+func authMiddleware(h http.Handler) http.Handler {
+	log.Println("Inflating AuthMiddleWare")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h.ServeHTTP(w, r)
 		// w.WriteHeader(400)
@@ -16,20 +26,66 @@ func AuthMiddleware(h http.Handler, comment string) http.Handler {
 }
 
 //Inflates the Function to HTTP verb map
-func ResourceHandler(muxer map[string]func(w http.ResponseWriter, r *http.Request), comment string) http.Handler {
-	log.Println("Inflating ResourceHandler for " + comment)
+func (epm EndPointMux) ResourceHandler() http.Handler {
+	log.Println("Inflating ResourceHandler")
 	wrappedIndex := make(map[string]http.Handler)
-	for method, fun := range muxer {
-		wrappedIndex[method] = AuthMiddleware(http.HandlerFunc(fun), method)
+
+	if epm.GET != nil {
+		log.Println("GETTER INFLATED")
+		wrappedIndex[http.MethodGet] = authMiddleware(http.HandlerFunc(epm.GET))
 	}
+
+	if epm.POST != nil {
+		log.Println("POSTER INFLATED")
+		wrappedIndex[http.MethodPost] = authMiddleware(http.HandlerFunc(epm.POST))
+	}
+
+	if epm.PUT != nil {
+		log.Println("PUTTER INFLATED")
+		wrappedIndex[http.MethodPut] = authMiddleware(http.HandlerFunc(epm.PUT))
+	}
+
+	if epm.PATCH != nil {
+		log.Println("PATCH INFLATED")
+		wrappedIndex[http.MethodPatch] = authMiddleware(http.HandlerFunc(epm.PATCH))
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("ResourceHandler Invoked for " + r.Method + " on " + comment)
+		log.Println("ResourceHandler Invoked for " + r.Method)
 
 		switch r.Method {
 		case http.MethodGet:
-			wrappedIndex[http.MethodGet].ServeHTTP(w, r)
+			if epm.GET != nil {
+				wrappedIndex[http.MethodGet].ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+			}
+
 		case http.MethodPost:
-			wrappedIndex[http.MethodPost].ServeHTTP(w, r)
+			if epm.POST != nil {
+				wrappedIndex[http.MethodPost].ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+			}
+
+		case http.MethodPut:
+			if epm.PUT != nil {
+				wrappedIndex[http.MethodPut].ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+			}
+		case http.MethodDelete:
+			if epm.DELETE!= nil {
+				wrappedIndex[http.MethodDelete].ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+			}
+		case http.MethodPatch:
+			if epm.PATCH!= nil {
+				wrappedIndex[http.MethodPatch].ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+			}
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 
@@ -38,18 +94,9 @@ func ResourceHandler(muxer map[string]func(w http.ResponseWriter, r *http.Reques
 
 }
 
-func MakeMap() map[string]func(w http.ResponseWriter, r *http.Request) {
-	return make(map[string]func(w http.ResponseWriter, r *http.Request))
-}
-
-func GET(muxer map[string]func(w http.ResponseWriter, r *http.Request), f func(w http.ResponseWriter, r *http.Request)) map[string]func(w http.ResponseWriter, r *http.Request) {
-	muxer[http.MethodGet] = f
-	return muxer
-}
-
-func POST(muxer map[string]func(w http.ResponseWriter, r *http.Request), f func(w http.ResponseWriter, r *http.Request)) map[string]func(w http.ResponseWriter, r *http.Request) {
-	muxer[http.MethodPost] = f
-	return muxer
+func CreateEndpoint(uri string) EndPointMux {
+	EPM := EndPointMux{URI:uri}
+	return EPM
 }
 
 // func sample(w http.ResponseWriter, r *http.Request) {
@@ -57,10 +104,17 @@ func POST(muxer map[string]func(w http.ResponseWriter, r *http.Request), f func(
 // }
 
 // func main() {
-// 	index := MakeMap()
-// 	index = GET(index, sample)
-// 	index = POST(index, sample)
-// 	http.Handle("/sample", ResourceHandler(index, "Sample Endpoint"))
-// 	http.ListenAndServe(":8092", nil)
+// 	mux := http.NewServeMux()
+
+// 	epm := CreateEndpoint("/sample")
+// 	epm2 := CreateEndpoint("/sample/kekre")
+
+// 	epm.GET = sample
+// 	epm2.POST = sample
+	
+// 	mux.Handle(epm.URI, epm.ResourceHandler())
+// 	mux.Handle(epm2.URI, epm.ResourceHandler())
+	
+// 	http.ListenAndServe(":8092", mux)
 // }
 
